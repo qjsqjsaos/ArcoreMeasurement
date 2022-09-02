760bcf96d963c84866f1da08bd19180cb2725063
qjsqjsaos <merrygoaround0726@gmail.com>
2022-09-02 18:15:19 +0900

    두 모드로 나누었으니 이어서 진행할 것 내 생각에는 어느정도 범위를 벗어날때 앵커 따라나기게 하는 모드로 변경하는등 유동적이여야 할 거 같다.

--- app/src/main/java/com/shibuiwilliam/arcoremeasurement/Measurement.kt
+++ app/src/main/java/com/shibuiwilliam/arcoremeasurement/Measurement.kt
@@ -10,9 +10,11 @@ import android.net.Uri
 import android.os.*
 import android.util.Log
 import android.view.PixelCopy
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.*
+import com.google.ar.core.exceptions.FatalException
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.FrameTime
 import com.google.ar.sceneform.Node
@@ -51,6 +53,8 @@ class Measurement : AppCompatActivity(), Scene.OnUpdateListener {
 
     private lateinit var clearButton: Button
 
+    private var mediateMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (!checkIsSupportedDeviceOrFinish(this)) {
@@ -61,7 +65,10 @@ class Measurement : AppCompatActivity(), Scene.OnUpdateListener {
         setContentView(R.layout.activity_measurement)
 
         arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment?
-        arFragment?.arSceneView?.scene?.addOnUpdateListener(this::onUpdateFrame)
+
+        arFragment?.arSceneView?.scene?.addOnUpdateListener(this@Measurement::onUpdateFrame)
+
+
 
         initCM = resources.getString(R.string.initCM)
 
@@ -100,10 +107,13 @@ class Measurement : AppCompatActivity(), Scene.OnUpdateListener {
     // TODO: 이거 원래 상태로 바꿀것
     private fun clearButton() {
         clearButton = findViewById(R.id.clearButton)
-//        clearButton.setOnClickListener { clearAllAnchors() }
         clearButton.setOnClickListener {
-            takePhoto()
+            mediateMode = !mediateMode
+            clearAllAnchors()
         }
+//        clearButton.setOnClickListener {
+//            takePhoto()
+//        }
     }
 
     private fun clearAllAnchors() {
@@ -169,73 +179,236 @@ class Measurement : AppCompatActivity(), Scene.OnUpdateListener {
         Constants.getImageUriQ(this, bitmap, isSave)
     }
 
+
+    //계속 호출되면서 앵커를 생성 (4개)
+//    private fun onUpdateFrame(frameTime: FrameTime?) {
+//
+//        val frame = arFragment!!.arSceneView.arFrame ?: return
+//
+//        // If there is no frame, just return.
+//
+//        //Making sure ARCore is tracking some feature points, makes the augmentation little stable.
+//        if (frame.camera.trackingState === TrackingState.TRACKING) {
+//
+//            val cameraPose = frame.camera.pose.extractRotation()
+//
+//            //위치
+//            val pos: Pose?
+//            // TODO: 회의실에서 이거 위치 조정하기 위치 조정 후에 범위 좁혀가기
+//            //4개의 앵커 위치 설정
+//            when (anchorCnt++) {
+//                1 -> {
+//                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
+//                }
+//                2 -> {
+//                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
+//                }
+//                3 -> {
+//                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
+//                }
+//                4 -> {
+//                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
+//                }
+//                else -> {
+////                    clearAllAnchors()
+//                    anchorCnt = 1
+//                    return
+//                }
+//            }
+//
+//
+//            val anchor = arFragment!!.arSceneView.session!!.createAnchor(pos)
+//            placedAnchors.add(anchor)
+//            val anchorNode = AnchorNode(anchor).apply {
+//                isSmoothed = true
+//                setParent(arFragment!!.arSceneView.scene)
+//            }
+//            placedAnchorNodes.add(anchorNode)
+//
+//            // Create the arrow node and add it to the anchor.
+////            val arrow = Node()
+////            arrow.setParent(anchorNode)
+////            arrow.setParent(anchorNode)
+////            arrow.renderable = cubeRenderable
+//
+//
+//            val node = TransformableNode(arFragment!!.transformationSystem)
+//                .apply {
+//                    this.rotationController.isEnabled = false
+//                    this.scaleController.isEnabled = false
+//                    this.translationController.isEnabled = true
+//                    this.renderable = renderable
+//                    setParent(anchorNode)
+//                }
+//            node.renderable = cubeRenderable
+//            arFragment!!.arSceneView.scene.addChild(anchorNode)
+//            node.select()
+//        }
+//    }
+
+    private var transformableNode: TransformableNode? = null
     //앵커 갯수
     var anchorCnt = 1
 
-    //계속 호출되면서 앵커를 생성 (4개)
+    private var oldFirstNodePose: Pose? = null
+    private var oldSecondNodePose: Pose? = null
+    private var oldThirdNodePose: Pose? = null
+    private var oldFourNodePose: Pose? = null
+
+
     private fun onUpdateFrame(frameTime: FrameTime?) {
+        arFragment?.let { fragment ->
+            fragment.arSceneView?.let { sceneView ->
+                sceneView.arFrame?.let { frame ->
+                    if (placedAnchors.size < 4) {
 
-        val frame = arFragment!!.arSceneView.arFrame ?: return
+                        if(anchorCnt == 4) anchorCnt == 1
 
-        // If there is no frame, just return.
+                        val trackable = frame.getUpdatedTrackables(Plane::class.java).iterator()
+                        if (trackable.hasNext()) {
+                            val plane = trackable.next() as Plane
+                            if (plane.trackingState == TrackingState.TRACKING) {
+                                fragment.planeDiscoveryController?.hide()
+                                val hitTest =
+                                    frame.hitTest(frame.screenCenter().x, frame.screenCenter().y)
+                                val hitTestIterator = hitTest.iterator()
+                                if (hitTestIterator.hasNext()) {
+                                    //전에 포즈가 있었다면
+                                    val pose: Pose
+                                    if(
+                                        oldFirstNodePose != null ||
+                                        oldSecondNodePose != null ||
+                                        oldThirdNodePose != null ||
+                                        oldFourNodePose != null
+                                            ) {
+                                        pose = when(anchorCnt++) {
+                                            1 -> oldFirstNodePose!!
+                                            2 -> oldSecondNodePose!!
+                                            3 -> oldThirdNodePose!!
+                                            else -> oldFourNodePose!!
+                                        }
+                                        
+                                    } else {
+                                        val hitResult = hitTestIterator.next()
+                                        pose = hitResult.hitPose
+                                    }
 
-        //Making sure ARCore is tracking some feature points, makes the augmentation little stable.
-        if (frame.camera.trackingState === TrackingState.TRACKING) {
+                                    val modelAnchor = plane.createAnchor(pose)
+                                    placedAnchors.add(modelAnchor)
+                                    val anchorNode = AnchorNode(modelAnchor).apply {
+                                        isSmoothed = true
+                                        setParent(arFragment!!.arSceneView.scene)
+                                    }
+                                    placedAnchorNodes.add(anchorNode)
+                                    anchorNode.setParent(sceneView.scene)
 
-            val cameraPose = frame.camera.pose.extractRotation()
+                                    transformableNode = TransformableNode(arFragment!!.transformationSystem)
+                                        .apply {
+                                            this.rotationController.isEnabled = false
+                                            this.scaleController.isEnabled = false
+                                            this.translationController.isEnabled = true
+                                            this.renderable = renderable
+                                            setParent(anchorNode)
+                                        }
+                                    transformableNode!!.setParent(anchorNode)
+                                    transformableNode!!.renderable = this@Measurement.cubeRenderable
 
-            //위치
-            val pos: Pose?
-            // TODO: 회의실에서 이거 위치 조정하기 위치 조정 후에 범위 좁혀가기
-            //4개의 앵커 위치 설정
-            when (anchorCnt++) {
-                1 -> {
-                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
-                }
-                2 -> {
-                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
-                }
-                3 -> {
-                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
-                }
-                4 -> {
-                    pos = cameraPose.compose(Pose.makeTranslation(0f,0f,-2f))
-                }
-                else -> {
-                    clearAllAnchors()
-                    anchorCnt = 1
-                    return
+
+                                    transformableNode!!.worldPosition = Vector3(
+                                        modelAnchor.pose.tx(),
+                                        modelAnchor.pose.compose(
+                                            Pose.makeTranslation(
+                                                0f,
+                                                0.05f,
+                                                0f
+                                            )
+                                        ).ty(),
+                                        modelAnchor.pose.tz()
+                                    )
+                                } else { }
+                            } else { }
+                        } else { }
+
+                    } else {
+
+                        //조정하기 버튼을 만든 후에 불리언으로 (조정하기) (앵커 따라다니게 하기) 두가지 모드를 만드는 편이 나을 듯 하다.
+                        // TODO: 두 모드로 나누었으니 이어서 진행할 것 내 생각에는 어느정도 범위를 벗어날때 앵커 따라나기게 하는 모드로 변경하는등 유동적이여야 할 거 같다. 
+                        if(placedAnchorNodes.isNotEmpty() && !mediateMode) {
+                            val frame = arFragment!!.arSceneView.arFrame ?: return
+                            if (frame.camera.trackingState === TrackingState.TRACKING) {
+                                val cameraPose = frame.camera.pose.extractRotation()
+                                val first = cameraPose.compose(Pose.makeTranslation(0f,0f,-1f))
+                                val second = cameraPose.compose(Pose.makeTranslation(0.3f,0f,-.8f))
+                                val third = cameraPose.compose(Pose.makeTranslation(0f,0.3f,-1f))
+                                val four = cameraPose.compose(Pose.makeTranslation(0.3f,0.3f,-1f))
+                                try {
+                                    //앵커 4개 따라다니게 하기
+                                    placedAnchorNodes[0] = moveRenderable(placedAnchorNodes[0], first, 0)!!
+                                    oldFirstNodePose = placedAnchorNodes[0].anchor?.pose
+
+                                    placedAnchorNodes[1] = moveRenderable(placedAnchorNodes[1], second, 1)!!
+                                    oldSecondNodePose = placedAnchorNodes[1].anchor?.pose
+
+                                    placedAnchorNodes[2] = moveRenderable(placedAnchorNodes[2], third, 2)!!
+                                    oldThirdNodePose = placedAnchorNodes[2].anchor?.pose
+
+                                    placedAnchorNodes[3] = moveRenderable(placedAnchorNodes[3], four, 3)!!
+                                    oldFourNodePose = placedAnchorNodes[3].anchor?.pose
+
+                                }catch (e: FatalException) {
+
+                                }
+
+                            }
+                        }
+                    }
                 }
             }
-
-
-            val anchor = arFragment!!.arSceneView.session!!.createAnchor(pos)
-            placedAnchors.add(anchor)
-            val anchorNode = AnchorNode(anchor).apply {
-                isSmoothed = true
-                setParent(arFragment!!.arSceneView.scene)
-            }
-            placedAnchorNodes.add(anchorNode)
-
-            // Create the arrow node and add it to the anchor.
-//            val arrow = Node()
-//            arrow.setParent(anchorNode)
-//            arrow.setParent(anchorNode)
-//            arrow.renderable = cubeRenderable
-
-
-            val node = TransformableNode(arFragment!!.transformationSystem)
-                .apply {
-                    this.rotationController.isEnabled = false
-                    this.scaleController.isEnabled = false
-                    this.translationController.isEnabled = true
-                    this.renderable = renderable
-                    setParent(anchorNode)
-                }
-            node.renderable = cubeRenderable
-            arFragment!!.arSceneView.scene.addChild(anchorNode)
-            node.select()
         }
+    }
+
+    private fun beforeHasNode(anchorCnt: Int) {
+
+    }
+
+    private fun moveRenderable(
+        markAnchorNodeToMove: AnchorNode?,
+        newPoseToMoveTo: Pose,
+        index: Int
+    ): AnchorNode? {
+        //Move a renderable to a new pose
+        if (markAnchorNodeToMove != null) {
+            arFragment!!.arSceneView.scene.removeChild(markAnchorNodeToMove)
+            placedAnchorNodes.remove(markAnchorNodeToMove)
+        } else {
+            return null
+        }
+        val frame = arFragment!!.arSceneView.arFrame
+        val session = arFragment!!.arSceneView.session
+        val markAnchor = session!!.createAnchor(newPoseToMoveTo.extractTranslation())
+        val newMarkAnchorNode = AnchorNode(markAnchor)
+        newMarkAnchorNode.renderable = cubeRenderable
+        newMarkAnchorNode.setParent(arFragment!!.arSceneView.scene)
+        placedAnchorNodes.add(index, newMarkAnchorNode)
+
+        //Delete the line if it is drawn
+        removeLine(transformableNode!!)
+        return newMarkAnchorNode
+    }
+
+    private fun removeLine(lineToRemove: Node) {
+        //remove the line
+        var lineToRemove: Node? = lineToRemove
+        if (lineToRemove != null) {
+            arFragment!!.arSceneView.scene.removeChild(lineToRemove)
+            lineToRemove.setParent(null)
+            lineToRemove = null
+        }
+    }
+
+    private fun Frame.screenCenter(): Vector3 {
+        val vw = findViewById<View>(android.R.id.content)
+        return Vector3(vw.width / 2f, vw.height / 2f, 0f)
     }
 
     private fun placeAnchor(
